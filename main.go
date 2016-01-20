867d9e41282b66c8c9b4826919884acf7c34769c
Manuel Franco <mfranco@flexiant.com>
2016-01-20 18:13:34 +0100

    Example of templating

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"path/filepath"
+	"text/template"
 
 	"github.com/mafraba/digger"
 	"gopkg.in/yaml.v2"
@@ -19,16 +21,59 @@ func main() {
 		panic(err)
 	}
 
-	roleList := `{"rc" : { "frontend" : { "number" : 3 }}}`
-	roleListDigger, _ := digger.NewJSONDigger([]byte(roleList))
+	roleList := `
+	{
+		"rc" : {
+			"frontend" : {
+				"number" : 3,
+				"container": {
+					"name": "contname",
+					"image": "somerepo/someimage",
+					"version": "latest",
+					"port": 8080
+				}
+			}
+		}
+	}
+	`
+	roleListDigger, err := digger.NewJSONDigger([]byte(roleList))
+	if err != nil {
+		panic(err)
+	}
 	attributes, _ := digger.NewMultiDigger(
 		roleListDigger,
 		defaults,
 	)
 
-	path := "rc/frontend/number"
-	val, _ := attributes.GetNumber(path)
-	fmt.Printf("%s -> %v", path, val)
+	tmpl := parseTemplate("./samples/frontend-controller.yaml", attributes)
+
+	// Run the template to verify the output.
+	err = tmpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalf("error executing template: %s", err)
+	}
+
+}
+
+func parseTemplate(templatePath string, attributes digger.Digger) *template.Template {
+	templateFile, _ := filepath.Abs(templatePath)
+	templateContent, err := ioutil.ReadFile(templateFile)
+	if err != nil {
+		panic(err)
+	}
+	// First we create a FuncMap with which to register the function.
+	funcMap := template.FuncMap{
+		// Associating functions with aliases
+		"getString": attributes.GetString,
+		"getNumber": attributes.GetNumber,
+		"getBool":   attributes.GetBool,
+	}
+	// Create a template, add the function map, and parse the text.
+	tmpl, err := template.New("templateTest").Funcs(funcMap).Parse(string(templateContent))
+	if err != nil {
+		log.Fatalf("error parsing: %s", err)
+	}
+	return tmpl
 }
 
 func parseMetadata() Metadata {
