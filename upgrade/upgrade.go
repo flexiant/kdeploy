fbecaee3f18d3289117110b32c001b0a89ead3da
Manuel Franco <mfranco@flexiant.com>
2016-02-04 16:39:27 +0100

    Check if newer version already deployed before upgrading

--- upgrade/upgrade.go
+++ upgrade/upgrade.go
@@ -9,6 +9,7 @@ import (
 	"github.com/flexiant/kdeploy/upgrade/strategies"
 	"github.com/flexiant/kdeploy/utils"
 	"github.com/flexiant/kdeploy/webservice"
+	"github.com/hashicorp/go-version"
 )
 
 // CmdUpgrade implements 'upgrade' command
@@ -28,11 +29,18 @@ func CmdUpgrade(c *cli.Context) {
 	// labelSelector := fmt.Sprintf("kubeware=%s,kubeware-version=%s", md.Name, md.Version)
 
 	// Check if kubeware already installed, error if it's not
-	version, err := kubernetes.FindDeployedKubewareVersion(namespace, md.Name)
+	v, err := kubernetes.FindDeployedKubewareVersion(namespace, md.Name)
 	utils.CheckError(err)
-	log.Infof("Found version %s of %s.%s", version, namespace, md.Name)
+	log.Infof("Found version %s of %s.%s", v, namespace, md.Name)
 
-	// TODO: Check if version equal or newer one already exists, error if so
+	// Check if equal or newer version already exists, error if so
+	deployedVersion, err := version.NewVersion(v)
+	utils.CheckError(err)
+	upgradeVersion, err := version.NewVersion(md.Version)
+	utils.CheckError(err)
+	if upgradeVersion.LessThan(deployedVersion) {
+		log.Fatalf("Can not upgrade to version '%s' since version '%s' is already deployed", md.Version, v)
+	}
 
 	// build attributes merging "role list" to defaults
 	log.Debugf("Building attributes")
